d505438b20ceeadc2158c423108bf4adfba0df24
Brayan Lopera <lopera616@gmail.com>
2024-08-07 14:59:37 -0500

    Se organiza formulario

--- trazab/src/Shared/Formulario.js
+++ trazab/src/Shared/Formulario.js
@@ -8,20 +8,20 @@ const formulario = () => {
     <div className="formularioPrincipal">
         <form className="form0">
             <div className="row">
-            <div className='col-3'>
-            <label className='form-label' htmlFor="campo1">Nombres</label>
-            <input className='form-control' type="text" id="campo1"/>
-            </div>
+                <div className='col-3'>
+                <label className='form-label' htmlFor="campo1">Nombres</label>
+                <input className='form-control' type="text" id="campo1"/>
+                </div>
             
-            <div className='col-3'>
-            <label className='form-label' htmlFor="campo2">Apellidos</label>
-            <input className='form-control' type="text" id="campo2"/>
-            </div>
+                <div className='col-3'>
+                <label className='form-label' htmlFor="campo2">Apellidos</label>
+                <input className='form-control' type="text" id="campo2"/>
+                </div>
 
-            <div className='col-3'>
-            <label className='form-label' htmlFor="campo3">Identificación</label>
-            <input className='form-control' type="text" id="campo3"/>
-            </div>
+                <div className='col-3'>
+                <label className='form-label' htmlFor="campo3">Identificación</label>
+                <input className='form-control' type="text" id="campo3"/>
+                </div>
             </div>
             <button type="button" class='btn btn-primary'>Aceptar</button>
         </form>
